1640f795fd5699a7318f4be1de8a25ee8b125996
Yash Awari <mr.yash3110@gmail.com>
2026-07-31 22:48:24 +0530

    changes in code

--- cpp_programs/classwork/hierarchical_inheritance2.cpp
+++ cpp_programs/classwork/hierarchical_inheritance2.cpp
@@ -4,11 +4,11 @@ class Vehicle
 {
     protected:
     std::string vehicletype;
-    float weight;
+    float rate;
 
 
-    Vehicle():vehicletype("no vehicle"),weight(0){std::cout<<"Vehicle default constructor called"<<std::endl; }
-    Vehicle(std::string type,float weigh):vehicletype(type),weight(weigh){std::cout<<"Vehicle paramterised constructor called"<<std::endl;}
+    Vehicle():vehicletype("no vehicle"),rate(0){std::cout<<"Vehicle default constructor called"<<std::endl; }
+    Vehicle(std::string type,float _rate):vehicletype(type),rate(_rate){std::cout<<"Vehicle paramterised constructor called"<<std::endl;}
 
 };
 
@@ -18,12 +18,17 @@ class Car:public Vehicle
 
   public:
   Car():Vehicle(){std::cout<<"Car default constructor invoked"<<std::endl;}
-  Car(std::string type,float weigh):Vehicle(type,weigh){std::cout<<"Car parameterised constructor invoked"<<std::endl;}
+  Car(std::string type,float _rate):Vehicle(type,_rate){std::cout<<"Car parameterised constructor invoked"<<std::endl;}
 
-  void Calculatefare(int hrs,float km)
+  void Calculatefare(int hrs)
   {
-    fare=hrs*300+km*10;
-    std::cout<<"Estimated Fare cost by car is:\tRs."<<fare<<std::endl;
+    fare=hrs*300+rate;
+    std::cout<<"Estimated fare cost by Car is:\tRs."<<fare<<"\t for"<<hrs<<"hrs"<<std::endl;
+  }
+  void Calculatefare(double km)
+  {
+    fare=50*km+rate;
+    std::cout<<"Estimated fare cost by Car is:\tRs."<<fare<<"\t for"<<km<<"km"<<std::endl;
   }
 }; 
 
@@ -33,23 +38,31 @@ class Bike:public Vehicle
 
     public:
     Bike():Vehicle(){std::cout<<"Bike default constructor invoked"<<std::endl;}
-    Bike(std::string type,float weigh):Vehicle(type,weigh){std::cout<<"Bike parameterised constructor invoked"<<std::endl;}
+    Bike(std::string type,float _rate):Vehicle(type,_rate){std::cout<<"Bike parameterised constructor invoked"<<std::endl;}
 
-    void Calculatefare(int hrs,float km)
+    void Calculatefare(int hrs)
     {
-        fare=hrs*100+km*10;
-        std::cout<<"Estimated fare cost by bike is:\tRs."<<fare<<std::endl;
+        fare=hrs*100+rate;
+        std::cout<<"Estimated fare cost by bike is:\tRs."<<fare<<"\t for"<<hrs<<"hrs"<<std::endl;
+
+    }
+    void Calculatefare(double km)
+    {
+        fare=25*km+rate;
+        std::cout<<"Estimated fare cost by bike is:\tRs."<<fare<<"\t for"<<km<<"km"<<std::endl;
 
     }
 };
 
 int main()
 {
-    Car c1("SUV",2000);
-    c1.Calculatefare(5,90.5);
+    Car c1("SUV",2500);
+    c1.Calculatefare(7);
+    c1.Calculatefare(90.5);
 
-    Bike b1("Cruiser",200);
-    b1.Calculatefare(8,200);
+    Bike b1("Cruiser",1000);
+    b1.Calculatefare(8);
+    b1.Calculatefare(300.0);
 }
 
 
